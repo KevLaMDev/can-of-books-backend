208d574e9c9dde620e956504847f49688d80fcb9
KEVIN LAMARCA <LAMARCA.K@GMAIL.COM>
2022-01-24 19:30:45 -0500

    setup complete

--- server.js
+++ server.js
@@ -3,11 +3,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL)
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  console.log('Mongoose is connected')
+});
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.get('/test', (request, response) => {
 
@@ -15,4 +24,3 @@ app.get('/test', (request, response) => {
 
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
